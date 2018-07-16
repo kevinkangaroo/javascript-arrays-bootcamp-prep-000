7368f69302f2cb7406427fa97dcb08ce706456fd
Kevin Kang <kevkang@live.com>
2018-07-16 04:56:47 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,17 +1,21 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 
 function addElementToBeginningOfArray(array, element) {
+  // No altering
   return [element, ...array]
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
+  // Alters
   return array.unshift(element)
 }
 
 function addElementToEndOfArray(array, element) {
+  // No altering
   return [..array, element]
 }
 
 function addElementToEndOfArray(array, element) {
+  // Alters
   return 
 }
