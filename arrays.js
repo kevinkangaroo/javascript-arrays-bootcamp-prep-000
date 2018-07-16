f79e04c76ab8a9b1160ef833507de2f71083745e
Kevin Kang <kevkang@live.com>
2018-07-16 05:06:12 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -19,3 +19,23 @@ function addElementToEndOfArray(myArray, element) {
   // Alters
   return myArray.push(element)
 }
+
+function accessElementInArray(myArray, index) {
+  return myArray[index]
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(myArray) {
+  return myArray.shift()
+}
+
+function removeElementFromBeginningOfArray(myArray) {
+  return myArray.slice(1)
+}
+
+function destructivelyRemoveElementFromEndOfArray(myArray) {
+  myArray.pop()
+}
+
+function removeElementFromEndOfArray(myArray) {
+  return myArray.slice(0, myArray.length - 1)
+}
